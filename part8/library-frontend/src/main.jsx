8c6a98aa9a85b730827bcb764b83af4a136e2f94
Pedro Maia <pedromaia@MacBook-Pro-de-Pedro-2.local>
2026-05-11 04:34:35 -0300

    Initial submission

--- part8/library-frontend/src/main.jsx
+++ part8/library-frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import { ApolloProvider } from '@apollo/client/react'
 import App from './App.jsx'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
   cache: new InMemoryCache(),
 })
 
